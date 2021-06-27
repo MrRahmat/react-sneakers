bbbac90eb74bfc9d13181b99fb602cdb1f0ef98f
MrRahmat <vovacet12@mail.ru>
2021-06-28 01:41:41 +0800

    Вывод Card с помощью массива

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,33 @@ import Card from './components/Card';
 import Header from './components/Header';
 import Cart from './components/Cart';
  
+const arr = [
+  {
+    title: 'Мужские кроссовки Nike Blazer Mid Suede',
+    price: 12999,
+    imgUrl: '/img/sneakers/1.jpg'
+  },
+  {
+    title: 'Мужские кроссовки Nike Air Max 270',
+    price: 14999,
+    imgUrl: '/img/sneakers/2.jpg'
+  },
+  {
+    title: 'Мужские кроссовки Nike Blazer Mid Suede',
+    price: 8499,
+    imgUrl: '/img/sneakers/3.jpg'
+  },
+  {
+    title: 'Кроссовки Puma X Aka Boku Future Rider',
+    price: 8999,
+    imgUrl: '/img/sneakers/4.jpg'
+  }
+];
+
 function App() {
   return (
     <div className="wrapper">
-      <Cart/>
+      <Cart arr/>
 
       <Header/>
       <div className="content">
@@ -18,11 +41,13 @@ function App() {
         </div>
   
         <div className="sneakers">
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
+          {arr.map((obj) => (
+            <Card
+            title={obj.title}
+            price={obj.price}
+            imgUrl={obj.imgUrl}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-function Card(params) {
+function Card(props) {
     return(
         <div className="card">
             <div className="cardFavourite">
               <img src="/img/heart-unliked.svg" alt="Unlike"/> 
             </div>
-            <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers"/>
-            <h5>Мужские кроссовки Nike Blazer Mid Suede</h5>
+            <img width={133} height={112} src={props.imgUrl} alt="Sneakers"/>
+            <h5>{props.title}</h5>
             <div className="cardBottom">
               <div className="cardCost">
                 <p>Цена:</p>
-                <b>12 999 руб.</b>
+                <b>{props.price} руб.</b>
               </div>
               <button className="cardBtn">
                 <img width={11} height={11} src="/img/plus.svg" alt="+"/>
